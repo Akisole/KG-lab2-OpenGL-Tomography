7b2eef555786a242e4ce35e05148ade991accbca
Akisole <32121754+Akisole@users.noreply.github.com>
2018-03-19 10:12:10 +0300

    Add files via upload

---
+++ KG-lab2-OpenGL-Tomography/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KG_lab2_OpenGL_Tomography
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
